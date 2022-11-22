3100b7e7a1183cbcd566f5c929b72be2288041cf
fpwilliams71 <fpwilliams71@yahoo.com>
2022-11-22 19:17:04 +0000

    dded

--- azurerm_windows_virtual_machine/azure-windows-vm.tf
+++ azurerm_windows_virtual_machine/azure-windows-vm.tf
@@ -12,12 +12,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "TestResourceGroup"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "test-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
